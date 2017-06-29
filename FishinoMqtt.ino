9a8cbd85ffb888cf4985fa182df932bf3eee97ed
gb <gico.gmail.com>
2017-06-29 18:59:35 +0200

    first commit

---
+++ FishinoMqtt.ino
@@ -0,0 +1,208 @@
+// This example uses a Fishino Uno Board
+// to connect to shiftr.io.
+//
+// You can check on your device after a successful
+// connection here: https://shiftr.io/try.
+//
+// by Giovanni Bonomini based on the 
+// ESP32 Development Board example of Joël Gähwiler
+// https://github.com/256dpi/arduino-mqtt
+
+#include <Fishino.h>
+#include <SPI.h>
+#include <MQTTClient.h>
+//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
+// CONFIGURATION DATA		-- ADAPT TO YOUR NETWORK !!!
+// DATI DI CONFIGURAZIONE	-- ADATTARE ALLA PROPRIA RETE WiFi !!!
+#ifndef __MY_NETWORK_H
+
+// here pur SSID of your network
+// inserire qui lo SSID della rete WiFi
+#define MY_SSID	"FASTWEB-1"
+
+// here put PASSWORD of your network. Use "" if none
+// inserire qui la PASSWORD della rete WiFi -- Usare "" se la rete non ￨ protetta
+#define MY_PASS	"x12wdx12wd"
+
+// here put required IP address (and maybe gateway and netmask!) of your Fishino
+// comment out this lines if you want AUTO IP (dhcp)
+// NOTE : if you use auto IP you must find it somehow !
+// inserire qui l'IP desiderato ed eventualmente gateway e netmask per il fishino
+// commentare le linee sotto se si vuole l'IP automatico
+// nota : se si utilizza l'IP automatico, occorre un metodo per trovarlo !
+//#define IPADDR	192, 168,   1, 251
+#define GATEWAY	192, 168,   1, 1
+#define NETMASK	255, 255, 255, 0
+#define DNSSRV  192, 168,   1, 1
+#endif
+//                    END OF CONFIGURATION DATA                      //
+//                       FINE CONFIGURAZIONE                         //
+///////////////////////////////////////////////////////////////////////
+
+// define ip address if required
+// NOTE : if your network is not of type 255.255.255.0 or your gateway is not xx.xx.xx.1
+// you should set also both netmask and gateway
+#ifdef IPADDR
+	IPAddress ip(IPADDR);
+	#ifdef GATEWAY
+		IPAddress gw(GATEWAY);
+	#else
+		IPAddress gw(ip[0], ip[1], ip[2], 1);
+	#endif
+	#ifdef NETMASK
+		IPAddress nm(NETMASK);
+	#else
+		IPAddress nm(255, 255, 255, 0);
+	#endif
+  #ifdef DNSSRV
+    IPAddress dn(DNSSRV);
+  #else
+    IPAddress dn(ip[0], ip[1], ip[2], 1);
+  #endif
+#endif
+
+// Initialize the Fishino client library
+#define SECURE
+#ifdef SECURE
+  FishinoSecureClient client;
+#else
+  FishinoClient client;
+#endif
+
+MQTTClient mqtt_client;
+
+// last time you connected to the server, in milliseconds
+// l'ultima volta che vi siete connessi al server, in millisecondi
+unsigned long lastConnectionTime = 0;
+
+//last publish time
+unsigned long lastMillis = 0;
+
+void printWifiStatus()
+{
+	// print the SSID of the network you're attached to:
+	// stampa lo SSID della rete:
+	Serial.print("SSID: ");
+	Serial.println(Fishino.SSID());
+
+	// print your WiFi shield's IP address:
+	// stampa l'indirizzo IP della rete:
+	IPAddress ip = Fishino.localIP();
+	Serial << F("IP Address: ");
+	Serial.println(ip);
+
+	// print the received signal strength:
+	// stampa la potenza del segnale di rete:
+	long rssi = Fishino.RSSI();
+	Serial << F("signal strength (RSSI):");
+	Serial.print(rssi);
+	Serial << F(" dBm\n");
+}
+
+
+void connect() {
+  Serial.print("checking wifi...");
+  while (Fishino.status() != STATION_GOT_IP) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.print("\nconnecting...");
+  while (!mqtt_client.connect("arduino", "try", "try")) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nconnected!");
+
+  mqtt_client.subscribe("/hello");
+  // client.unsubscribe("/hello");
+}
+
+void setup()
+{
+	// Initialize serial and wait for port to open
+	// Inizializza la porta seriale e ne attende l'apertura
+	Serial.begin(115200);
+	
+	// only for Leonardo needed
+	// necessario solo per la Leonardo
+	while (!Serial)
+		;
+
+	// initialize SPI
+	// inizializza il modulo SPI
+//	SPI.begin();
+//	SPI.setClockDivider(SPI_CLOCK_DIV2);
+	
+	// reset and test WiFi module
+	// resetta e testa il modulo WiFi
+	while(!Fishino.reset())
+		Serial << F("Fishino RESET FAILED, RETRYING...\n");
+	Serial << F("Fishino WiFi RESET OK\n");
+
+	// go into station mode
+	// imposta la modalità stazione
+	Fishino.setMode(STATION_MODE);
+
+	// try forever to connect to AP
+	// tenta la connessione finchè non riesce
+	Serial << F("Connecting to AP...");
+	while(!Fishino.begin(MY_SSID, MY_PASS))
+	{
+		Serial << ".";
+		delay(2000);
+	}
+	Serial << "OK\n";
+	
+	
+	// setup IP or start DHCP client
+	// imposta l'IP statico oppure avvia il client DHCP
+#ifdef IPADDR
+	Fishino.config(ip, dn, gw, nm);
+#else
+	Fishino.staStartDHCP();
+#endif
+
+	// wait till connection is established
+	Serial << F("Waiting for IP...");
+	while(Fishino.status() != STATION_GOT_IP)
+	{
+		Serial << ".";
+		delay(500);
+	}
+	Serial << "OK\n";
+
+	
+	// print connection status on serial port
+	// stampa lo stato della connessione sulla porta seriale
+	printWifiStatus();
+  #ifdef SECURE
+    mqtt_client.begin("broker.shiftr.io",8883, client);
+  #else
+    mqtt_client.begin("broker.shiftr.io",8883, client);
+  #endif
+  mqtt_client.onMessage(messageReceived);
+  connect();
+}
+
+void loop()
+{
+	mqtt_client.loop();
+  delay(10);  // <- fixes some issues with WiFi stability
+
+  if (!mqtt_client.connected()) {
+    connect();
+  }
+
+  // publish a message roughly every second.
+  if (millis() - lastMillis > 1000) {
+    lastMillis = millis();
+    mqtt_client.publish("/hello", "world");
+  }
+}
+
+void messageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+}
